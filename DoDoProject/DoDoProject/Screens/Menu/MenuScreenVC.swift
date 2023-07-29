f5caf7d1b5dcc5273597720ac731772d3a75c217
Arsenty Halimovsky <ghost12@yandex.ru>
2023-07-29 13:46:14 +0300

    Create MenuScreenVC

---
+++ DoDoProject/DoDoProject/Screens/Menu/MenuScreenVC.swift
@@ -0,0 +1,19 @@
+//
+//  MenuScreenVC.swift
+//  DoDoProject
+//
+//  Created by Арсентий Халимовский on 29.07.2023.
+//
+
+import UIKit
+
+final class MenuScreenVC: UIViewController {
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // some code here
+    }
+}
