f18047c13be7d0b1e70408d53557d1dc6aa4a33e
Arsenty Halimovsky <ghost12@yandex.ru>
2023-07-29 13:51:59 +0300

    Create ConctactsScreenVC

---
+++ DoDoProject/DoDoProject/Screens/Contacts/ContactsScreenVC.swift
@@ -0,0 +1,19 @@
+//
+//  ContactsScreenVC.swift
+//  DoDoProject
+//
+//  Created by Арсентий Халимовский on 29.07.2023.
+//
+
+import UIKit
+
+final class ContactsScreenVC: UIViewController {
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // some code here
+    }
+}
