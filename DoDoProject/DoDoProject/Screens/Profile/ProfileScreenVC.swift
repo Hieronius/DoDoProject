3885376b3172ef17c36b645d499c747fffe64894
Arsenty Halimovsky <ghost12@yandex.ru>
2023-07-29 13:50:13 +0300

    Create ProfileScreenVC

---
+++ DoDoProject/DoDoProject/Screens/Profile/ProfileScreenVC.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileScreenVC.swift
+//  DoDoProject
+//
+//  Created by Арсентий Халимовский on 29.07.2023.
+//
+
+import UIKit
+
+final class ProfileScreenVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // some code here
+    }
+}
