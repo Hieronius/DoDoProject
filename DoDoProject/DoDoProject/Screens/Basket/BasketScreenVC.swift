5450278e839dc48f2787c00017821d2c0a40a6fe
Arsenty Halimovsky <ghost12@yandex.ru>
2023-07-29 13:53:11 +0300

    Create BasketScreenVC

---
+++ DoDoProject/DoDoProject/Screens/Basket/BasketScreenVC.swift
@@ -0,0 +1,19 @@
+//
+//  BasketScreenVC.swift
+//  DoDoProject
+//
+//  Created by Арсентий Халимовский on 29.07.2023.
+//
+
+import UIKit
+
+final class BasketScreenVC: UIViewController {
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // some code here
+    }
+}
