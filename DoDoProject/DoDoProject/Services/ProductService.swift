99fdc5b282573a6bd388bb59003636bea020ee96
Arsenty Halimovsky <ghost12@yandex.ru>
2023-07-30 15:40:44 +0300

    Create ProductService

---
+++ DoDoProject/DoDoProject/Services/ProductService.swift
@@ -0,0 +1,31 @@
+//
+//  ProductService.swift
+//  DoDoProject
+//
+//  Created by Арсентий Халимовский on 30.07.2023.
+//
+
+import Foundation
+
+final class ProductService {
+    
+    // MARK: - Private Properties
+
+    private let products: [Product] = [
+        Product(name: "Гавайская", detail: "Тесто, Сыр, Буженина", price: 590, image: "hawaii", type: "product", count: 0),
+        Product(name: "Маргарита", detail: "Тесто, Сыр, Колбаска", price: 650, image: "margarita", type: "product", count: 0),
+        Product(name: "Пепперони", detail: "Тесто, Сыр, Перец, Томат, Лук", price: 710, image: "pepperoni", type: "product", count: 0),
+        Product(name: "Гавайская", detail: "Тесто, Сыр, Буженина", price: 590, image: "hawaii", type: "product", count: 0),
+        Product(name: "Маргарита", detail: "Тесто, Сыр, Колбаска", price: 650, image: "margarita", type: "product", count: 0),
+        Product(name: "Пепперони", detail: "Тесто, Сыр, Перец, Томат, Лук", price: 710, image: "pepperoni", type: "product", count: 0),
+        Product(name: "Гавайская", detail: "Тесто, Сыр, Буженина", price: 590, image: "hawaii", type: "product", count: 0),
+        Product(name: "Маргарита", detail: "Тесто, Сыр, Колбаска", price: 650, image: "margarita", type: "product", count: 0),
+        Product(name: "Пепперони", detail: "Тесто, Сыр, Перец, Томат, Лук", price: 710, image: "pepperoni", type: "product", count: 0)
+    ]
+
+    // MARK: - Public Methods
+
+    func fetchProducts() -> [Product] {
+        return products
+    }
+}
