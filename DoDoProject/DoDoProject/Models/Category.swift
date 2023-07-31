bdd1127d88134c0671b36a6666c8d5a7098d1003
Arsenty Halimovsky <ghost12@yandex.ru>
2023-07-31 16:39:46 +0300

    Add category model

---
+++ DoDoProject/DoDoProject/Models/Category.swift
@@ -0,0 +1,13 @@
+//
+//  Category.swift
+//  DoDoProject
+//
+//  Created by Арсентий Халимовский on 31.07.2023.
+//
+
+import Foundation
+
+struct Category {
+    var name: String
+    var type: String
+}
