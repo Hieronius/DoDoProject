df87c86a9d0c406b944fda6c844e3d7278fcb418
Arsenty Halimovsky <ghost12@yandex.ru>
2023-08-02 15:22:06 +0300

    Implement category service

---
+++ DoDoProject/DoDoProject/Services/CategoryService.swift
@@ -0,0 +1,30 @@
+//
+//  CategoryService.swift
+//  DoDoProject
+//
+//  Created by Арсентий Халимовский on 02.08.2023.
+//
+
+import Foundation
+
+final class CategoryService {
+    
+    // MARK: - Private Properties
+    
+    private let categories: [Category] = [
+        Category(name: "Пицца", type: "category"),
+        Category(name: "Комбо", type: "category"),
+        Category(name: "Кофе", type: "category"),
+        Category(name: "Закуски", type: "category"),
+        Category(name: "Десерты", type: "category"),
+        Category(name: "Напитки", type: "category"),
+        Category(name: "Соусы", type: "category"),
+        Category(name: "Другие товары", type: "category")
+    ]
+    
+    // MARK: Public Methods
+    
+    func fetchCategories() -> [Category] {
+        return categories
+    }
+}
